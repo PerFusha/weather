8c1d62e78a7235fa5ce272372d945f23129a1306
Weather <yfenics@gmail.com>
2025-12-08 00:46:20 +0300

    добавлен ph-pages

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+    publicPath: `/weather/`,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
